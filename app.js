02b0ed0b0db26d40c2438896d15493075135a595
Alexander Urban <lexa@toxa.de>
2020-05-22 13:55:36 +0200

    Refactoring: Interfaces and POC use lib/getStatus.js

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-/* eslint-env mocha */
-"use strict";
+/* eslint-disable no-console */
 
-const utilities = require("./lib/utilities");
+require('./lib/utilities')
 
-const err = undefined;
-console.log(err == null);
+const err = undefined
+console.log(err == null)
 
-const err1 = undefined;
-console.log(err1 == null);
+const err1 = undefined
+console.log(err1 == null)
 
-const err2 = {};
-console.log(err2 == null);
+const err2 = {}
+console.log(err2 == null)
