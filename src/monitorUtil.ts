98e5a951de1ba41358334476e0311b6050c217b2
Niklas Sandström <mr.n.sandstrom@gmail.com>
2023-04-04 13:54:49 +0200

    add types for 'MonitoredSystem'

--- src/monitorUtil.ts
+++ src/monitorUtil.ts
@@ -1,6 +1,38 @@
 import type { Request, Response } from 'express'
 
-const monitorSystems = async (req: Request, res: Response, monitoredSystems?: [any]): Promise<void> => {
+interface BasicSystem {
+  key: string
+  required?: boolean
+}
+
+interface IsResolvedSystem extends BasicSystem {
+  isResolved: boolean
+  message: string
+}
+
+interface MongoSystem extends BasicSystem {
+  db: any
+}
+
+interface RedisSystem extends BasicSystem {
+  redis: any
+}
+
+interface SqlSystem extends BasicSystem {
+  db: any
+}
+
+interface CustomCheckSystem extends BasicSystem {
+  getStatus: Function
+}
+
+type MonitoredSystem = MongoSystem | IsResolvedSystem | RedisSystem | SqlSystem | CustomCheckSystem
+
+export const monitorSystems = async (
+  req: Request,
+  res: Response,
+  monitoredSystems?: MonitoredSystem[]
+): Promise<void> => {
   const contentType = req.headers.accept
 
   if (req?.headers?.accept === 'application/json') res.json({ message: 'OK' })
